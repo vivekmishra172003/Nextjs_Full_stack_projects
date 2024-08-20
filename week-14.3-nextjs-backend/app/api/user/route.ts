6310a6ee1c1072d7f084f5c47eef8ad8ddd123d1
vivekmishra172003 <vivekmishra172003@gmail.com>
2024-08-21 00:38:47 +0530

    now come here after learing prisma

--- week-14.3-nextjs-backend/app/api/user/route.ts
+++ week-14.3-nextjs-backend/app/api/user/route.ts
@@ -10,12 +10,8 @@ export function GET(req:NextRequest){
 export async function POST(req:NextRequest){
     //body
 const body = await req.json();
-//header
-console.log(req.headers.get("authorization"));
-// query parameters
-console.log(req.nextUrl.searchParams.get("name"));
-//hit the database with username and passowrd
+
 return NextResponse.json({
-    message:"YOu are signed up "
+    body
 })
 }
--- week-14.3-nextjs-backend/components/Signup.tsx
+++ week-14.3-nextjs-backend/components/Signup.tsx
@@ -1,7 +1,49 @@
-export default function Signup(){
-    return<>
-    <input type="text" name="" id="" placeholder="name" />
-    <input type="password" name="" id="" placeholder="password" />
-    <button>submit</button>
-    </>
+"use client"
+
+import axios from "axios";
+import { useState } from "react";
+
+export default function Signup() {
+    const [username,setUsername] = useState<String>("");
+    const [password,setPassword] = useState<String>("");
+    return (
+    <div className="flex flex-col justify-center h-screen">
+        <div className="flex justify-center">
+        <div className="border rounded pb-4 p-4">
+            <input
+            onChange={(e)=>{
+                setUsername(e.target.value);
+            }}
+            className="p-2 m-2"
+            type="text"
+            name=""
+            id=""
+            placeholder="name"
+            />
+            <br />
+            <input
+            onChange={(e)=>{
+                setPassword(e.target.value);
+            }}
+            className="p-2 m-2"
+            type="password"
+            name=""
+            id=""
+            placeholder="password"
+            />
+            <br />
+            <div className="flex justify-center">
+            <button className="mt-4" 
+            onClick={()=>{
+                axios.post("http://localhost:3000/api/user/",{
+                    username,
+                    password
+                })
+            }}
+            >submit</button>
+            </div>
+        </div>
+        </div>
+    </div>
+    );
 }
